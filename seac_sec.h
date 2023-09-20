14981444b701947de9d74838eb5338a9e1161ecb
Nicolas LG <34491677+N-LG@users.noreply.github.com>
2023-09-21 00:03:31 +0200

    Add files via upload

--- seac_sec.h
+++ seac_sec.h
@@ -73,7 +73,7 @@ void ipv6S2B(char *entree,char *sortie){
                   :"eax");
 }
 
-//convertit adresse ethernet chaine vers binaire
+//convertit adresse ethernet binaire vers chaine
 void etherB2S(char *entree,char *sortie){
     asm volatile ("mov   $108, %%eax\n"
     		  "int   $0x61\n"
@@ -82,7 +82,7 @@ void etherB2S(char *entree,char *sortie){
                   :"eax");
 }
 
-//convertit adresse ipv4 chaine vers binaire
+//convertit adresse ipv4 binaire vers chaine
 void ipv4B2S(char *entree,char *sortie){
     asm volatile ("mov   $109, %%eax\n"
     		  "int   $0x61\n"
@@ -91,7 +91,7 @@ void ipv4B2S(char *entree,char *sortie){
                   :"eax");
 }
 
-//convertit adresse ipv6 chaine vers binaire
+//convertit adresse ipv6 binaire vers chaine
 void ipv6B2S(char *entree,char *sortie){
     asm volatile ("mov   $110, %%eax\n"
     		  "int   $0x61\n"
@@ -99,3 +99,32 @@ void ipv6B2S(char *entree,char *sortie){
                   :"edx"(entree),"ecx"(sortie)
                   :"eax");
 }
+
+//convertit une chaine codé en base 64 en binaire
+void base64binaire(char *entree,char *sortie, long taille){
+    asm volatile ("mov   $116, %%eax\n"
+    		  "int   $0x61\n"
+    	          : /* no output */
+                  :"eS"(entree),"eD"(sortie),"ecx"(taille)
+                  :"eax");
+}
+
+//convertit des données binaire en base64 de base
+void base64gen(char *entree,char *sortie, long taille){
+    asm volatile ("mov   $117, %%eax\n"
+    		  "int   $0x61\n"
+    	          : /* no output */
+                  :"eS"(entree),"eD"(sortie),"ecx"(taille)
+                  :"eax");
+}
+
+//convertit des données binaire en base64 adapté aux url
+void base64url(char *entree,char *sortie, long taille){
+    asm volatile ("mov   $118, %%eax\n"
+    		  "int   $0x61\n"
+    	          : /* no output */
+                  :"eS"(entree),"eD"(sortie),"ecx"(taille)
+                  :"eax");
+}
+
+
